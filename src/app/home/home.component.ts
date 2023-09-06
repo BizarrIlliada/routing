1ffdd2a79b7c572299d69e701a9288dcd3abe6a2
BizarrIlliada <kazkoviy@icloud.com>
2023-09-07 01:58:06 +0300

    routes, params

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -7,7 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
+
+  toServers() {
+    console.log('Servers page is active');
+
+    this.router.navigate(['/servers'])
+  }
 
   ngOnInit() {
   }
--- src/app/users/user/user.component.ts
+++ src/app/users/user/user.component.ts
@@ -1,16 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
   styleUrls: ['./user.component.css']
 })
-export class UserComponent implements OnInit {
-  user: {id: number, name: string};
+export class UserComponent implements OnInit, OnDestroy {
+  user: { id: number, name: string };
+  paramsSubscription: Subscription;
 
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute
+  ) { }
 
   ngOnInit() {
+    //only if the component have to be reloaded from within, use code below (instead using this.route.snapshot.params...)
+    this.paramsSubscription = this.route.params.subscribe((params: Params) => {
+      const { id, name } = params;
+
+      this.user = { id, name }
+    })
   }
 
+  ngOnDestroy(): void {
+    this.paramsSubscription.unsubscribe();
+  } // Angular does it instead of us, so we don't need to unsubscribe manually
 }
